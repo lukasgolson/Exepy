dd68f39327cbf544efb06264eb10c791c47ea829
Lukas Olson <lukasdragon@live.ca>
2024-02-21 15:15:38 -0800

    Fixed error messages when paths not as expected

--- main.go
+++ main.go
@@ -17,27 +17,27 @@ func main() {
 
 	settings, err := common.LoadOrSaveDefault(settingsFileName)
 	if err != nil {
-		panic(err)
+		return
 	}
 
 	pythonScriptPath := path.Join(settings.PayloadDir, settings.PayloadScript)
 	requirementsPath := path.Join(settings.PayloadDir, settings.RequirementsFile)
 
 	// check if payload directory exists
-	if common.DoesPathExist(settings.PayloadDir) {
+	if !common.DoesPathExist(settings.PayloadDir) {
 		println("Payload directory does not exist: ", settings.PayloadDir)
 		return
 	}
 
 	// check if payload directory has the main file
-	if common.DoesPathExist(pythonScriptPath) {
+	if !common.DoesPathExist(pythonScriptPath) {
 		println("Main file does not exist: ", pythonScriptPath)
 		return
 	}
 
 	// if requirements file is listed, check that it exists
 	if settings.RequirementsFile != "" {
-		if common.DoesPathExist(requirementsPath) {
+		if !common.DoesPathExist(requirementsPath) {
 			println("Requirements file is listed in config but does not exist: ", requirementsPath)
 			return
 		}
