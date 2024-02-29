666bc714cd55bb5c2639aa788d84cec08b8f500c
Lukas Olson <lukasdragon@live.ca>
2024-02-28 16:39:29 -0800

    More small working directory fixes

--- common/CommandRunner.go
+++ common/CommandRunner.go
@@ -6,11 +6,17 @@ import (
 )
 
 func RunCommand(command string, args []string) error {
+	return RunCommandInDir(command, args, "")
+}
+
+func RunCommandInDir(command string, args []string, dir string) error {
 	cmd := exec.Command(command, args...)
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
+	cmd.Dir = dir
 
 	println("Running command:", cmd.String())
 	return cmd.Run()
